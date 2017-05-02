43ec6829ef6aec5f72698da2185230a31a76ec99
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-05-02 17:10:31 -0400

    making AJAX request to get JSON response

--- cats.js
+++ cats.js
@@ -2,6 +2,10 @@ $(function(){
 
   $('.summon-cats').on('click', function() {
     console.log("button works")
+    $.ajax({
+      url: 'http://bitkittens.herokuapp.com/cats.json',
+      method: 'GET'
+    });
   });
 
 
