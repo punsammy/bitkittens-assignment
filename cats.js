6b030047a8c8ac6ae409e0b1706b09e0034cbf2d
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-05-02 17:30:48 -0400

    Summon Cats button adds all three cats at the same time

--- cats.js
+++ cats.js
@@ -4,7 +4,13 @@ $(function(){
     console.log("button works")
     $.ajax({
       url: 'http://bitkittens.herokuapp.com/cats.json',
-      method: 'GET'
+      method: 'GET',
+      // dataType: 'json'
+    }).done(function() {
+      $('<img>').attr('src', 'https://s3.amazonaws.com/bitmakerhq/resources/web-development/bitkittens/20160817_141210.jpeg').appendTo('#cat1')
+      $('<img>').attr('src', 'https://s3.amazonaws.com/bitmakerhq/resources/web-development/bitkittens/moxie.jpg').appendTo('#cat2')
+      $('<img>').attr('src', 'https://s3.amazonaws.com/bitmakerhq/resources/web-development/bitkittens/Bagheera_Mufasa.jpg').appendTo('#cat3')
+
     });
   });
 
