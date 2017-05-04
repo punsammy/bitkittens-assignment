5d504958580b6ab489de046f9a7ac3545eaea67f
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-05-04 00:52:09 -0400

    Bitkittens complete

--- cats.js
+++ cats.js
@@ -1,18 +1,45 @@
 $(function(){
 
   $('.summon-cats').on('click', function() {
-    console.log("button works")
+    // console.log("button works")
     $.ajax({
       url: 'http://bitkittens.herokuapp.com/cats.json',
       method: 'GET',
-      // dataType: 'json'
-    }).done(function() {
-      $('<img>').attr('src', 'https://s3.amazonaws.com/bitmakerhq/resources/web-development/bitkittens/20160817_141210.jpeg').appendTo('#cat1')
+      dataType: 'json'
+    }).done(function(data) {
+
+      $('<img>').attr('src', 'https://s3.amazonaws.com/bitmakerhq/resources/web-development/bitkittens/20160817_141210.jpeg').appendTo('#cat1');
       $('<img>').attr('src', 'https://s3.amazonaws.com/bitmakerhq/resources/web-development/bitkittens/moxie.jpg').appendTo('#cat2')
       $('<img>').attr('src', 'https://s3.amazonaws.com/bitmakerhq/resources/web-development/bitkittens/Bagheera_Mufasa.jpg').appendTo('#cat3')
 
     });
+
   });
 
-
 });
+
+// var catContainer = $('<div>');
+//
+// var catPicSrc = 'https://s3.amazonaws.com/bitmakerhq/resources/web-development/bitkittens/' + cat.address;
+// $('<img>').attr('src', catPicSrc).appendTo(catContainer);
+//
+// // $('<h2>').html(cat.photo).appendTo(catContainer);
+//
+// console.log('Clicked on Cat link')
+
+// Attempt at doing it in one line
+// $('img').attr('src', 'https://s3.amazonaws.com/bitmakerhq/resources/web-development/bitkittens/20160817_141210.jpeg').appendTo($('<body>').('div'))
+// One way of doing it. Not most efficient.
+// $('<img>').attr('src', 'https://s3.amazonaws.com/bitmakerhq/resources/web-development/bitkittens/moxie.jpg').appendTo('#cat2')
+// $('<img>').attr('src', 'https://s3.amazonaws.com/bitmakerhq/resources/web-development/bitkittens/Bagheera_Mufasa.jpg').appendTo('#cat3')
+// $('<img>').attr('src', 'https://s3.amazonaws.com/bitmakerhq/resources/web-development/bitkittens/20160817_141210.jpeg').appendTo(id);
+
+
+// Another way
+// var catBox = data['cats'];
+// for (var i = 0; i < catBox.length; i++) {
+//   cat = catBox[i]
+//   id = '#cat' + (i + 1)
+//
+//   $('<img>').attr('src', cat.photo ).appendTo(id)
+// }
